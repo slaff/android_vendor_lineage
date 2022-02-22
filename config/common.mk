d219e78f5d41ad7fc9283affeada171b4544b4d1
Dimitris Katikaridis <dkatikaridis@gmail.com>
2022-02-23 00:04:28 +0200

    common: Disable adb authentication

--- config/common.mk
+++ config/common.mk
@@ -13,16 +13,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
-else
-# Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
-
-# Disable extra StrictMode features on all non-engineering builds
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
-endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
